ec86f889842f93b7860b60de3df56325a086e45b
Marcos Antonio Senn <marcossenn@gmail.com>
2023-11-23 18:50:33 -0300

    landing page de nike

---
+++ src/Sections/Services.jsx
@@ -0,0 +1,13 @@
+import { services } from "../Constants";
+import ServiceCard from "../Components/ServiceCard.jsx";
+const Services = () => {
+    return (
+        <section className="max-container flex justify-center flex-wrap gap-9">
+            {services.map((service) => (
+                <ServiceCard key="service.label" {...service} />
+            ))}
+        </section>
+    );
+};
+
+export default Services;
